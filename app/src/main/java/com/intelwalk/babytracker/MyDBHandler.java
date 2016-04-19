b23f2a2126ab49d4520f024819e82c50f3c4a31b
intelwalk <intelwalk@gmail.com>
2016-04-19 11:40:04 -0500

    All input activities have been added.

--- app/src/main/java/com/intelwalk/babytracker/MyDBHandler.java
+++ app/src/main/java/com/intelwalk/babytracker/MyDBHandler.java
@@ -13,6 +13,20 @@ public class MyDBHandler extends SQLiteOpenHelper{
     public static final String TABLE_NAME = "events";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_EVENTNAME = "eventname";
+    public static final String COLUMN_EVENTDATE = "eventdate";
+    public static final String COLUMN_EVENTTIME = "eventtime";
+    public static final String COLUMN_EVENTCOMMENTS = "eventcomments";
+    public static final String COLUMN_CRYINGTYPE = "cryingtype";
+    public static final String COLUMN_WETDIRTYDIAPER = "wetdirtydiaper";
+    public static final String COLUMN_DIAPERRASH = "diaperrash";
+    public static final String COLUMN_NURSINGORFOOD = "nursingorfood";
+    public static final String COLUMN_NURSINGLORR = "nursinglorr";
+    public static final String COLUMN_TABLEORBABYFOOD = "tableorbabyfood";
+    public static final String COLUMN_TEMPERATURETYPE = "temperaturetype";
+    public static final String COLUMN_TEMPERATURENUMBER = "temperaturenumber";
+    public static final String COLUMN_NAPORNIGHTSSLEEP = "napornightssleep";
+    public static final String COLUMN_NAPSLEEPDURATION = "napsleepduration";
+    public static final String COLUMN_HAIRWASH = "hairwash";
 
     //We need to pass database information along to the superclass
     public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory, int version){
@@ -24,13 +38,27 @@ public class MyDBHandler extends SQLiteOpenHelper{
         String query = "CREATE TABLE " + TABLE_NAME + "(" +
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_EVENTNAME + " TEXT " +
+                COLUMN_EVENTDATE + " TEXT " +
+                COLUMN_EVENTTIME + " TEXT " +
+                COLUMN_EVENTCOMMENTS + " TEXT " +
+                COLUMN_CRYINGTYPE + " TEXT " +
+                COLUMN_WETDIRTYDIAPER + " INTEGER " +
+                COLUMN_DIAPERRASH + " INTEGER " +
+                COLUMN_NURSINGORFOOD + " INTEGER " +
+                COLUMN_NURSINGLORR + " INTEGER " +
+                COLUMN_TABLEORBABYFOOD + " INTEGER " +
+                COLUMN_TEMPERATURETYPE + " TEXT " +
+                COLUMN_TEMPERATURENUMBER + " REAL " +
+                COLUMN_NAPORNIGHTSSLEEP + " INTEGER " +
+                COLUMN_NAPSLEEPDURATION + " TEXT " +
+                COLUMN_HAIRWASH + " INTEGER " +
                 ");";
         db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        d
+
     }
 
 }
